4dce22807af9447c96f2e3e0d60318a5d126144e
Peter-Pander <172821797+Peter-Pander@users.noreply.github.com>
2024-07-25 19:30:56 +0200

    Write the logic to get the computer choice
    Create a new function named getComputerChoice.
    Write the code so that getComputerChoice
    will randomly return one of the following string values:
    “rock”, “paper” or “scissors”.
    Test the function using console.log.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,19 @@
-console.log("Hello World")
+// Create a function named getComputerChoice
+function getComputerChoice() {
+  // Create the variable "randomNum" and store the Math.random() method in it.
+  // The Math.random method returns a random number that’s greater than or equal to 0 and less than 1.
+  // This created number is therefore stored in randomNum. 
+  let randomNum = Math.random();
+  // if randomNum returns a number <= 1/3 return “rock”
+  // if randomNum returns a number > 1/3 and <= 2/3 return “paper”
+  // if randomNum returns a number > 2/3 return "scissors"
+  if (randomNum <= 1 / 3) {
+    return "rock";
+  } else if (randomNum > 1 / 3 && randomNum <= 2 / 3) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+// Test the function
+console.log(getComputerChoice());
